10f91528019a25571e5f08157d955115096c78ed
DPIVietnam <contact@dpi.asia>
2026-04-10 22:50:43 +0700

    update

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123@localhost/off_system"
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = "postgresql://user:CBIWQl1RRBiBrf0hSC6kDHONgNuILrSf@dpg-d7cbubm7r5hc73fncvjg-a.singapore-postgres.render.com/off_dpi"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = "super-secret-key-1234567890-very-long-key"
