30c6b63ef9449ae0d84ef56fbbc39852f20890fc
DPIVietnam <contact@dpi.asia>
2026-04-10 22:47:50 +0700

    update

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123@localhost/off_system"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123@localhost/off_system"
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = "super-secret-key-1234567890-very-long-key"
